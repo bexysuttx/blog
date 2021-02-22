a432856e1ed123ba842572b8ab819a303393b13d
Andrei Rodionov <andrei_rodionov@inbox.ru>
2021-02-22 16:38:17 +0300

    Add Error Handler

--- src/main/java/bexysuttx/blog/controller/page/ErrorController.java
+++ src/main/java/bexysuttx/blog/controller/page/ErrorController.java
@@ -8,16 +8,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import bexysuttx.blog.controller.AbstractController;
-import bexysuttx.blog.exception.ApplicationException;
-import bexysuttx.blog.exception.ValidateException;
 
-@WebServlet({"/news", "/news/*"})
-public class NewsController extends AbstractController {
-	private static final long serialVersionUID = 2617306785433062648L;
+@WebServlet("/error")
+public class ErrorController extends AbstractController {
+	private static final long serialVersionUID = -6350950464997810953L;
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		forwardToPage("news.jsp", req, resp);
+		forwardToPage("error.jsp", req, resp);
 	}
-
 }
---
+++ src/main/java/bexysuttx/blog/exception/ValidateException.java
@@ -0,0 +1,22 @@
+package bexysuttx.blog.exception;
+
+public class ValidateException extends Exception {
+	private static final long serialVersionUID = -137514416987051320L;
+
+	public ValidateException(String message) {
+		super(message);
+	}
+
+	public ValidateException(Throwable cause) {
+		super(cause);
+	}
+
+	public ValidateException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public ValidateException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+}
